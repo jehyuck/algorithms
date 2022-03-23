ceed80bbd6ff9abd0dc8155763265b3e96fd0b87
jehyuck <wpgur07@gmail.com>
2022-03-23 15:59:25 +0900

    문제풀이 업로드 1.DSLR#9019 시간줄이기

--- 백준풀이/dfsbfs/DSLR#9019.py
+++ 백준풀이/dfsbfs/DSLR#9019.py
@@ -17,31 +17,53 @@ for _ in range(T):
     visit = [1] * 10000
     start, target = map(int, input().split())
     que = d()
-    que.append((start, ""))
-    visit[start] = 0
-    n, s = (start, "")
+    que.append((start, 1))
+    visit[start] = 1
+    n, s = que.popleft()
 
     while n != target:
-        print(que)
         temp = (n * 2) % 10000
         if visit[temp]:
             visit[temp] = 0
-            que.append((temp, s + 'D'))
+            que.append((temp, s * 4 + 0))
         temp = n - 1 if n else 9999
         if visit[temp]:
             visit[temp] = 0
-            que.append((temp, s + 'S'))
+            que.append((temp, s * 4 + 1))
         a, b = divmod(n, 1000)
         temp = a + b * 10
         if visit[temp]:
             visit[temp] = 0
-            que.append((temp, s + 'L'))
+            que.append((temp, s * 4 + 2))
         a, b = divmod(n, 10)
         temp = a + b * 1000
         if visit[temp]:
             visit[temp] = 0
-            que.append((temp, s + 'R'))
+            que.append((temp, s * 4 + 3))
         n, s = que.popleft()
     answer.append(s)
 
-print(*answer, sep="\n")
+order = ("DSLR")
+def print_recur(num):
+    if num == 1:
+        return
+    d, m = divmod(num, 4)
+    print_recur(d)
+    print(order[m], end="")
+
+
+for i in answer:
+    print_recur(i)
+    print()
+
+
+"""
+시간을 줄이기 위해
+1. que속의 list --> tuple
+2. tuple을 언패킹해 모든 값들을 그 값으로 치환
+3. DSLR루트를 기록하기 위해 사용한 문자열 연산을 전부 제거
+(str) + (str) -> 'DSLR' + 'D' >{계속 문자열이 copy됨}
+ ㄴ->>형식을 아래의 형식으로
+ n * 4 + (0,1,2,3 중 택 1) (비트마스킹?? 형식)
+출력시 재귀 형식으로 끝에 나머지 부터 출력
+"""
